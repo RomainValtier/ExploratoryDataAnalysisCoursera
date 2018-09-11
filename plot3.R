d084ebb2b380d47a78d71aac6f15795f96ea944a
Romain Valtier <romain@valtier.fr>
2018-09-11 11:36:20 +0200

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,37 @@
+setwd("C:\\Users\\s8310533\\Documents\\GitHub\\ExploratoryDataAnalysisCoursera")
+
+# 0. Get data
+
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+fileNAME <- "household_power_consumption.zip"
+if (!file.exists(fileNAME)){
+        download.file(fileURL, fileNAME)
+}  
+if (!file.exists("household_power_consumption.txt")) { 
+        unzip(fileNAME)
+}
+fileNAME <- "household_power_consumption.txt"
+
+# 1. Load data
+plot3.data <- read.csv(fileNAME, header = TRUE, sep = ";", na.strings = "?")
+
+# 2. Convert Date
+plot3.data$Date <- as.Date(strptime(plot3.data$Date, format = "%d/%m/%Y"))
+
+# 3. Convert Time
+library(chron)
+plot3.data$Time <- times(plot3.data$Time)
+
+# 4. Select data from 2007-02-01 and 2007-02-02 
+plot3.data <- plot3.data[plot3.data$Date >= "2007-02-01" & plot3.data$Date <= "2007-02-02", ]
+
+str(plot3.data)
+
+# 5. Histogram Global Active Power
+png("plot3.png")
+Sys.setlocale("LC_TIME", "C")
+plot(as.POSIXct(paste(plot3.data$Date, plot3.data$Time)),plot3.data$Sub_metering_1, type = "l", col = "black", main = "", ylab = "Energy sub metering", xlab = "")
+lines(as.POSIXct(paste(plot3.data$Date, plot3.data$Time)),plot3.data$Sub_metering_2, type = "l", col = "red")
+lines(as.POSIXct(paste(plot3.data$Date, plot3.data$Time)),plot3.data$Sub_metering_3, type = "l", col = "blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1)
+dev.off()
